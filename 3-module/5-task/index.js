4d10ce70e5a6f9969821f4c3ee10582baa95f54a
Andriy U. Windmill <andriy.uvarov@windmill.ch>
2021-02-19 23:26:42 +0200

    [3-module] - Implemented the task 1-5. Updated the README.

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,8 @@
 function getMinMax(str) {
-  // ваш код...
+  let numbers = str.split(/,| /).filter(item => !isNaN(parseInt(item))).map(item => +item);
+  let result = {
+    min: Math.min(...numbers),
+    max: Math.max(...numbers),
+  };
+  return result;
 }
